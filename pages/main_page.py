5ff057998647a800d8a17b657a552a5cce4249c8
Варнава <i.varnava@pos-credit.ru>
2025-05-14 20:35:47 +0500

    Добавил pytest.ini и переаботал страницы locators,base,main,product

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,6 @@
+from .locators import MainPageLocators
+
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
-    def should_be_login_link(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-        login_link.click()
-
+    pass
