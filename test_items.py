c80ccb8411b97d8b82c241d7afe6ad9baa2de294
Ilya Varnava <104103128+Ilya19999@users.noreply.github.com>
2025-05-14 13:57:28 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,5 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--language", action="store", default="en", help="Choose language: en, fr, es, etc."
-    )
-
-@pytest.fixture(scope="function")
-def language(request):
-    return request.config.getoption("language")
+def test_open_page_with_language(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    # Простой assert на наличие кнопки "Добавить в корзину" (текст зависит от языка)
+    assert browser.find_element("css selector", ".btn-add-to-basket"), "Add to basket button not found"
